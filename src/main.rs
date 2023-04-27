c73c8726e9a62ba5a0577f26119e159390fb7601
Veronica <veronicatomilola@gmail.com>
2023-04-27 19:30:23 +0100

    Calculate relatedness

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,48 @@
+use bitcoin::{Address, Amount, OutPoint, TxIn, Txid};
+use bitcoin::hashes::hex::FromHex;
+use std::str::FromStr;
 mod utxos;
-// mod xpub;
 
 fn main() {
     use utxos::*;
-    // use xpub::relatedness_for_xpub;
 
     let utxo1 = Utxo {
-        txid: "d37fc44f31fc16a8b2e14b1a8531bb651f666312033db135076c22a9ba926dc9".to_string(),
-        vout: 1,
-        address: "bc1q0rr0p8cgrvte9d3q3lt83qgr3y65xkvh0s4h0z".to_string(),
-        amount: 100_000_000,
+        outpoint: OutPoint {
+            txid: Txid::from_hex("d37fc44f31fc16a8b2e14b1a8531bb651f666312033db135076c22a9ba926dc9").unwrap(),
+            vout: 1,
+        },
+        address: Address::from_str("2MxE7ujjouqUrTiUZReYJ98avwwbxyEp48z").unwrap(),
+        amount: Amount::from_sat(100_000_000),
         block_height: Some(710_000),
-        inputs: vec![("3d3a8107fb8e6e46f9f1d6d371b7dc8c729c03329d746f5aa5d5b5e5c5f93ec3".to_string(), 0)],
+        inputs: vec![TxIn {
+            previous_output: OutPoint {
+                txid: Txid::from_hex("3d3a8107fb8e6e46f9f1d6d371b7dc8c729c03329d746f5aa5d5b5e5c5f93ec3").unwrap(),
+                vout: 0,
+            },
+            sequence: 0,
+            witness: vec![],
+            script_sig: bitcoin::Script::new(),
+        }],
     };
 
     let utxo2 = Utxo {
-    txid: "e6a790b6f1c6b8ed450d8b2bbf9a5a5c5d5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a".to_string(),
-    vout: 0,
-    address: "bc1q4rr0p8cgrvte9d3q3lt83qgr3y65xkvh0s4h0z".to_string(),
-    amount: 50_000_000,
-    block_height: Some(720_000),
-    inputs: vec![("3d3a8107fb8e6e46f9f1d6d371b7dc8c729c03329d746f5aa5d5b5e5c5f93ec3".to_string(), 1)],
+        outpoint: OutPoint {
+            txid: Txid::from_hex("e6a790b6f1c6b8ed450d8b2bbf9a5a5c5d5a5a5a5a5a5a5a5a5a5a5a5a5a5a5a").unwrap(),
+            vout: 0,
+        },
+        address: Address::from_str("2MxE7ujjouqUrTiUZReYJ98avwwbxyEp48z").unwrap(),
+        amount: Amount::from_sat(50_000_000),
+        block_height: Some(720_000),
+        inputs: vec![TxIn {
+            previous_output: OutPoint {
+                txid: Txid::from_hex("3d3a8107fb8e6e46f9f1d6d371b7dc8c729c03329d746f5aa5d5b5e5c5f93ec3").unwrap(),
+                vout: 1,
+            },
+            sequence: 0,
+            witness: vec![],
+            script_sig: bitcoin::Script::new(),
+        }],
     };
 
-
     println!("{}", relatedness(utxo1, utxo2));
-
-    // let xpub = "xpub6C7hLiJp61qV7zyuFeJe1zV7JukPAgTQtrF9Y9XKDD7GxajWJq3zV57sBYsRSw7Vd8W4vegJvmsbYjJUb7HUhSpspjdwZgVjK77aA2FETMj".to_string();
-    // let related_pairs = relatedness_for_xpub(xpub);
-    // println!("{:?}", related_pairs);
-
 }
